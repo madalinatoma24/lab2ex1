e98c8a3e68f86210c668227d4c4ded7167d56242
madalina.toma <madalina.toma@softem.ro>
2022-12-05 11:12:54 +0200

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 1: Scrieti un program care va afisa suma cifrelor unui numar n, n fiind
+                   citit de la tastatura
+             */
+            Console.WriteLine("Introduceri numarul: ");
+            int n = int.Parse(Console.ReadLine());
+            var sum = 0;
+            while (n != 0)
+            {
+                sum = sum + (n % 10);
+                n = n / 10;
+            }
+
+            Console.WriteLine("Suma este:" + sum);
+
+
+        }
+    }
+}
